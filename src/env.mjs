fb2372080fbf1f50b4ffbefbcf003f3929d7adcf
Stephen Ulmer <stephenulmer@knights.ucf.edu>
2023-04-23 14:22:56 -0400

    revert env

--- src/env.mjs
+++ src/env.mjs
@@ -11,12 +11,20 @@ const server = z.object({
     process.env.NODE_ENV === "production"
       ? z.string().min(1)
       : z.string().min(1).optional(),
-  NEXTAUTH_URL: z.string().url(),
+  NEXTAUTH_URL: z
+    .preprocess(
+      (str) => "https://" + process.env.RAILWAY_STATIC_URL ?? str,
+      z.string().url()
+    )
+    .transform((x) => {
+      process.env.NEXTAUTH_URL = x;
+      return x;
+    }),
   GOOGLE_CLIENT_ID: z.string(),
   GOOGLE_CLIENT_SECRET: z.string(),
   // Add `.min(1) on ID and SECRET if you want to make sure they're not empty
-  DISCORD_CLIENT_ID: z.string(),
-  DISCORD_CLIENT_SECRET: z.string(),
+  // DISCORD_CLIENT_ID: z.string(),
+  // DISCORD_CLIENT_SECRET: z.string(),
 });
 
 /**
@@ -40,11 +48,10 @@ const processEnv = {
   NEXTAUTH_URL: process.env.NEXTAUTH_URL,
   GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
   GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
-  DISCORD_CLIENT_ID: process.env.DISCORD_CLIENT_ID,
-  DISCORD_CLIENT_SECRET: process.env.DISCORD_CLIENT_SECRET,
+  // DISCORD_CLIENT_ID: process.env.DISCORD_CLIENT_ID,
+  // DISCORD_CLIENT_SECRET: process.env.DISCORD_CLIENT_SECRET,
   // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
 };
-
 
 // Don't touch the part below
 // --------------------------
